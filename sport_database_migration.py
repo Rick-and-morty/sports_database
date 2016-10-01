e06c7637a8cfc31d101893208221c97d577a55c0
Rick-and-morty <te5840@gmail.com>
2016-10-01 15:02:38 -0400

    Sport data with fixes

--- sport_database_migration.py
+++ sport_database_migration.py
@@ -1,38 +1,42 @@
 import psycopg2
 import csv
-link = psycopg2.connect("dbname=sports_database user=sports_database")
+link = psycopg2.connect("dbname=sport_database user=sport_database")
+
 cursor = link.cursor()
-cursor.execute("DROP TABLE IF EXISTS sports_fun")
+cursor.execute("DROP TABLE IF EXISTS sport_database;")
+
 create_table = """
-CREATE TABLE sports_fun (
-id serial PRIMARY KEY,
-rushing,
-full_name varchar(20),
-att numeric(3),
-yds numeric(4),
-avg numeric(4),
-TD numeric(3),
-rec numeric(2),
-yds numeric(4),
-avg numeric(4),
-td numeric(2),
-plays numeric(3),
-yds numeric(4),
-avg numeric(4),
-td numeric(3),
+CREATE TABLE sport_database (
+    id serial PRIMARY KEY,
+    first_name varchar(20),
+    last_name varchar(20),
+    att int,
+    rushyds int,
+    rushavg int,
+    rushTD int,
+    rec int,
+    recyds int,
+    recavg int,
+    recTD int,
+    plays int,
+    scrimyds int,
+    scrimavg int,
+    scrimTD int
 );
 """
-cursor.execute(create_table)
-with open("sport_data") as open_file:
-    stats = csv.reader(open_file)
-    for row in stats:
-        cursor.execute("insert into sports_data values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s");",
-        (row[0]row[1]row[2]row[3]row[4]row[5]row[6]row[7]row[8]row[9]row[10]row[11]row[12]row[13]))
 
+cursor.execute(create_table)
+
+with open("sport_stats.csv") as open_file:
+    stats = csv.reader(open_file)
+
+    for row in stats:
+        cursor.execute("insert into sport_database values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);",
+            (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9],
+            row[10], row[11]))
 
 
 link.commit()
-
 cursor.close()
 
 link.close()
--- sports_database.py
+++ sports_database.py
@@ -1,19 +1,19 @@
 import psycopg2
-connection = psycopg2.connect("dbname=sport_database user=sports_data")
+
+connection = psycopg2.connect("dbname=sport_database")
 
 cursor = connection.cursor()
 
-name = input("what is the first name you want to search for?")
+name = input("what is the first name you want to search for?").title()
 
-password = input("Password? ")
+name1 = input("last name ").title()
 
-cursor.execute("SELECT * FROM person_data WHERE first_name = %s AND last_name = %s;" (name, password))
+cursor.execute("SELECT * FROM sport_database WHERE first_name = %s AND last_name = %s;", (name, name1))
 
 results = cursor.fetchall()
 
 print(results)
 
-
-
 cursor.close()
+
 connection.close()
